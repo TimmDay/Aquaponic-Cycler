f3eb02fe7f67b09bdab568ca4caf34275d5c2798
TimmDay <timmday.info@gmail.com>
2019-03-04 20:06:33 +0100

    firebase env var issue

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,12 +3,12 @@ require('firebase/auth');
 require("firebase/firestore");
 
 const config = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain:  process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL:  process.env.FIREBASE_DATABASE_URL,
-    projectId:  process.env.FIREBASE_PROJECT_ID,
-    storageBucket:  process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId:  process.env.FIREBASE_MESSAGING_SENDER_ID
+    apiKey: "AIzaSyC361HII1nxBAH61eyB_RNw50tOwgCh5TQ",
+    authDomain:  "course-d3-firebase.firebaseapp.com",
+    databaseURL:  "https://course-d3-firebase.firebaseio.com",
+    projectId:  "course-d3-firebase",
+    storageBucket:  "course-d3-firebase.appspot.com",
+    messagingSenderId:  "255578645190"
 };
 firebase.initializeApp(config);
 const db = firebase.firestore();
