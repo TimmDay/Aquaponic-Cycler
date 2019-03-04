40eb29d28a059b78962fdbfb290e19bc366a02ef
TimmDay <timmday.info@gmail.com>
2019-03-04 19:07:15 +0100

    include env vars for heroku dep

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -6,7 +6,7 @@ const config = {
     apiKey: process.env.FIREBASE_API_KEY,
     authDomain:  process.env.FIREBASE_AUTH_DOMAIN,
     databaseURL:  process.env.FIREBASE_DATABASE_URL,
-    projectId:  "course-d3-firebase",
+    projectId:  process.env.FIREBASE_PROJECT_ID,
     storageBucket:  process.env.FIREBASE_STORAGE_BUCKET,
     messagingSenderId:  process.env.FIREBASE_MESSAGING_SENDER_ID
 };
